ed52b11587c83c0464344d2baf617cd43ceccb97
Mingzhe <m.shi@student.tue.nl>
2022-08-08 10:36:59 +0200

    WIP: add code&dish data cache by `redis`

--- reggie_take_out/src/main/java/com/project/reggie/controller/UserController.java
+++ reggie_take_out/src/main/java/com/project/reggie/controller/UserController.java
@@ -9,6 +9,7 @@ import com.project.reggie.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
@@ -24,6 +26,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @PostMapping("/sendMsg")
     public R<String> sendMsg(@RequestBody User user, HttpSession session) {
@@ -40,6 +45,9 @@ public class UserController {
             //save generated code
             session.setAttribute(phone, code);
 
+            //将生成的验证码缓存到redis中，并设置时效5分钟
+//            redisTemplate.opsForValue().set(phone, code, 5, TimeUnit.MINUTES);
+
             return R.success("SMS MSg Send Success!");
         }
 
@@ -49,7 +57,7 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public R<User> sendMsg(@RequestBody Map map, HttpSession session) {
+    public R<User> login(@RequestBody Map map, HttpSession session) {
         log.info(map.toString());
         //get phone number
         String phone = map.get("phone").toString();
@@ -59,6 +67,9 @@ public class UserController {
 
         //get code from Session
 //        Object codeInSession = session.getAttribute(phone);
+
+        //从redis中获取缓存的验证码
+//        Object codeInsession = redisTemplate.opsForValue().get(phone);
 
         //compare verification code
 //        if (codeInSession != null && codeInSession.equals(code)) {
@@ -74,6 +85,11 @@ public class UserController {
                 userService.save(user);
             }
             session.setAttribute("user", user.getId());
+
+            //如果成功删除redis缓存
+//            redisTemplate.delete(phone);
+
+
             return R.success(user);
 
 //        }
